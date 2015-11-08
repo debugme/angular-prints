23744b5201e5e40f69927ecff218a325332c47ce
Asad Razvi <debugme@hotmail.com>
2015-11-08 15:37:52 +0000

    Minor formatting

--- gruntFile.js
+++ gruntFile.js
@@ -3,12 +3,12 @@ module.exports = function (grunt) {
     pngmin: {
       compile: {
         options: {
-          concurrency: 8,             // specify how many exucutables get spawned in parallel
-          colors: 128,                // reduce colors to 128
-          ext: '.png',                // use .png as extension for the optimized files
+          concurrency: 8,          // specify how many exucutables get spawned in parallel
+          colors: 128,             // reduce colors to 128
+          ext: '.png',             // use .png as extension for the optimized files
           quality: '50',           // output quality should be between 65 and 80 like jpeg quality
-          speed: 2,                  // pngquant should be as fast as possible
-          iebug: true                 // optimize image for use in Internet Explorer 6
+          speed: 2,                // pngquant should be as fast as possible
+          iebug: true              // optimize image for use in Internet Explorer 6
         },
         files: [
           {
